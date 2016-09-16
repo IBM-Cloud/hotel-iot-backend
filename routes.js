8964bb234c7ab2e377ef76b6f253ac4642d54732
Lindsay Roberts <lindsay.roberts@us.ibm.com>
2016-09-15 22:44:50 -0500

    Added Yelp API

--- routes.js
+++ routes.js
@@ -1,11 +1,19 @@
 var Twitter = require('twitter');
 var Watson = require('watson-developer-cloud');
+var Yelp = require('yelp');
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+var yelp = new Yelp({
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET,
 });
 
 var alchemy_language = new Watson.alchemy_language({
@@ -41,7 +49,25 @@ module.exports = function (app) {
           if(err) {
             console.log('Error:', err);
           } else {
-            res.status(200).json({username: req.params.username, latest_tweet: tweets[0], alchemy: response});
+            var keywords = '';
+            for(var i = 0; i < response.keywords.length; i++) {
+              keywords = keywords + response.keywords[i].text + ' ';
+            }
+            yelp.search({term: keywords, location: 'Austin'})
+            .then(function(data) {
+              res.status(200).json({keywords: keywords, data: data});
+            })
+            .catch(function(e) {
+              res.status(500).json({error: e});
+            });
+            // yelp.search({ term: 'food', location: 'Montreal' })
+            // .then(function (data) {
+            //   console.log(data);
+            // })
+            // .catch(function (err) {
+            //   console.error(err);
+            // });
+            // res.status(200).json({username: req.params.username, latest_tweet: tweets[0], alchemy: response});
           }
         });
         //res.status(200).json({tweets: tweets});
